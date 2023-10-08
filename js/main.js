6841945072ffb3c6f3dc257630c9f201ff2bc347
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2023-10-08 12:21:46 +0530

    added handtracking

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import * as THREE from 'three';
 import '../style.css'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -109,3 +112,132 @@ function animate() {
 }
 
 animate();
+
+let previousX=0;
+let selectedPlanet="earth";
+//handtracking
+async function setupCamera() {
+    try {
+        const video = document.getElementById('video');
+        const stream = await navigator.mediaDevices.getUserMedia({ 'video': true });
+        video.srcObject = stream;
+        video.style.display='none';
+        return new Promise((resolve) => {
+            video.onloadedmetadata = () => {
+                resolve(video);
+            };
+        });
+    } catch (error) {
+        console.error('Error accessing webcam:', error);
+    }
+  }
+  
+  async function loadHandposeModel() {
+      return await handpose.load();
+  }
+  
+  async function detectHands(video, model) {
+    const canvas1=document.querySelector("#canvas1");
+      const context = canvas1.getContext('2d');
+      const fingerJoints = {
+          thumb: [0, 1, 2, 3, 4],
+          indexFinger: [0, 5, 6, 7, 8],
+          middleFinger: [0, 9, 10, 11, 12],
+          ringFinger: [0, 13, 14, 15, 16],
+          pinky: [0, 17, 18, 19, 20],
+        };
+  
+      video.width = window.innerWidth;
+      video.height = window.innerHeight;
+      canvas1.width=video.width;
+      canvas1.height=video.height;
+  
+      const predictions = await model.estimateHands(video);
+        
+      context.clearRect(0, 0, canvas1.width, canvas1.height);
+  
+      if (predictions.length > 0) {
+          for (const hand of predictions) {
+              const landmarks = hand.landmarks;
+  
+              
+              context.fillStyle = 'white';
+              context.strokeStyle = 'white';
+              context.lineWidth = 2;
+              for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
+                  let finger = Object.keys(fingerJoints)[j];
+                  for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+  
+                    const firstJointIndex = fingerJoints[finger][k];
+                    const secondJointIndex = fingerJoints[finger][k + 1];
+  
+                    context.beginPath();
+                    context.moveTo(
+                      landmarks[firstJointIndex][0],
+                      landmarks[firstJointIndex][1]
+                    );
+                    context.lineTo(
+                      landmarks[secondJointIndex][0],
+                      landmarks[secondJointIndex][1]
+                    );
+                    context.stroke();
+                    
+                  }
+                }
+                for (let i = 0; i < landmarks.length; i++) {
+                  const x = landmarks[i][0];
+                  const y = landmarks[i][1];
+                  context.beginPath();
+                  context.arc(x, y, 3, 0, 3 * Math.PI);
+                  context.fill();
+                }
+  
+              
+          }
+      }
+      if (predictions.length > 0) {
+        const hand = predictions[0]; // Assuming you're tracking only one hand
+        const indexFingerTip = hand.landmarks[8]; // Index finger tip landmark
+        const [x, y] = indexFingerTip;
+
+        // Check if the hand is moving inward (decreasing X-coordinate)
+        if (x < previousX) {
+            // Cycle through planets based on hand movement
+            if (selectedPlanet === 'mars') {
+                selectedPlanet = 'earth';
+                console.log(selectedPlanet);
+                // Perform action for selecting Earth
+                //window.open('earth_experience.html', '_blank');
+            } else if (selectedPlanet === 'earth') {
+                selectedPlanet = 'moon';
+                console.log(selectedPlanet);
+                // Perform action for selecting Moon
+                //window.open('moon_experience.html', '_blank');
+            }
+            else{
+                selectedPlanet = 'mars';
+                console.log(selectedPlanet);
+            }
+            // Add additional conditions for other planets if needed
+
+            // Delay to prevent rapid selection on continuous hand movement
+            setTimeout(() => {
+                previousX = x;
+            }, 1000); // Adjust the delay as needed
+        }
+    }
+  
+      requestAnimationFrame(() => detectHands(video, model));
+  }
+  
+  async function run() {
+    try {
+        const video = await setupCamera();
+        const handposeModel = await loadHandposeModel();
+        await detectHands(video, handposeModel);
+    } catch (error) {
+        console.error('Error loading camera or handpose model:', error);
+    }
+  }
+  
+  run();
